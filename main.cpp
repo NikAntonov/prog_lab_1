9d8b9039c230807e3400e7cf8458adcd509133a1
Nick <nickantonovme@hotmail.com>
2018-09-17 14:49:06 +0300

    main.cpp: fix negative rome numbers print

--- main.cpp
+++ main.cpp
@@ -15,9 +15,9 @@ double F(double x) {
     else return (10*x)/(c-4);
 }
 
-void print(int number) {                                                                                 //Печать римских цифр
+void print(int number) {                                                                                    //Печать римских цифр
     char result[1000]{'\0'};                                                                                //Массив для хранения результата (вместо String)
-    const int values[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };                       //Позиции для римских цифр
+    const int values[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };                          //Позиции для римских цифр
     const char *symbols[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };      //Алфавит символов
     int counter = sizeof(symbols) / sizeof(symbols[0]) - 1;                                                 //Выбор последнего элемента массива
     while(counter >= 0) {                                                                                   //Цикл для перебора цифр числа и их замены
@@ -41,28 +41,26 @@ void output() {                                                                 
         //Проверка на условие с возвратом целых или вещественных чисел в функцию
         if ((int(a) && int(c)) || (int(b) && int(c)) != 0) result = F(i); else result = F(int(i));
 
+
         double result_forRome;
         if (result > 0) result_forRome = floor(result + 0.5); else result_forRome = ceil(result - 0.5);
 
         cout<<"["<<i<<"; "<<result<<"] : (";                                                                //Печать результата в арабских цифрах
-        print(int(i));                                                                                      //Печать римских цифр
+
+        if (i < 0) cout<<"-";
+        print(int(abs(i)));                                                                                      //Печать римских цифр
         cout<<"; ";
-        print(int(result_forRome));                                                                                 //Печать римских цифр
+        print(int(result_forRome));                                                                         //Печать римских цифр
         cout<<")"<<endl;                                                                                    //Сброс строки
     }
 }
 
 int main(){
-
     cout<<"INPUT: a -> b -> c -> x_start -> x_end -> dx"<<endl;
     cin>>a>>b>>c>>x_start>>x_end>>dx;
-
     cout<<"OUTPUT: X -> Y"<<endl;
-
     if (dx == 0) cout<<"ERROR: null dx";                                                                    //Обработка ошибки с бесконечным циклом
-
     output();
-
     return 0;
 }
 
